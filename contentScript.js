cb3bd6f129a78f55350650052e359f1c0392b50b
Tansel Berkant Oflaz <tanselberkant@gmail.com>
2022-06-15 12:26:15 +0300

    productionReady

--- contentScript.js
+++ contentScript.js
@@ -3,15 +3,6 @@
   let currentVideo = '';
   let currentVideoBookmarks = [];
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, videoId } = obj;
-
-    if (type === 'NEW') {
-      currentVideo = videoId;
-      newVideoLoaded();
-    }
-  });
-
   const fetchBookmarks = () => {
     return new Promise((resolve) => {
       chrome.storage.sync.get([currentVideo], (obj) => {
@@ -20,10 +11,27 @@
     });
   };
 
-  const newVideoLoaded = async () => {
-    const bookmarkBtnExists = document.getElementsByClassName('bookmark-btn')[0];
+  const addNewBookmarkEventHandler = async () => {
+    const currentTime = youtubePlayer.currentTime;
+    const newBookmark = {
+      time: currentTime,
+      desc: 'Bookmark at ' + getTime(currentTime),
+    };
+
     currentVideoBookmarks = await fetchBookmarks();
-    console.log(bookmarkBtnExists);
+
+    chrome.storage.sync.set({
+      [currentVideo]: JSON.stringify(
+        [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+      ),
+    });
+  };
+
+  const newVideoLoaded = async () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName('bookmark-btn')[0];
+
+    currentVideoBookmarks = await fetchBookmarks();
 
     if (!bookmarkBtnExists) {
       const bookmarkBtn = document.createElement('img');
@@ -36,33 +44,37 @@
         document.getElementsByClassName('ytp-left-controls')[0];
       youtubePlayer = document.getElementsByClassName('video-stream')[0];
 
-      youtubeLeftControls.append(bookmarkBtn);
+      youtubeLeftControls.appendChild(bookmarkBtn);
       bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
     }
   };
 
-  const addNewBookmarkEventHandler = () => {
-    const currentTime = youtubePlayer.currentTime;
-    const newBookmark = {
-      time: currentTime,
-      desc: 'Bookmark at ' + getTime(currentTime),
-    };
-    // console.log(newBookmark);
-    currentVideoBookmarks = await fetchBookmarks();
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
 
-    chrome.storage.sync.set({
-      [currentVideo]: JSON.stringify(
-        [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
-      ),
-    });
-  };
+    if (type === 'NEW') {
+      currentVideo = videoId;
+      newVideoLoaded();
+    } else if (type === 'PLAY') {
+      youtubePlayer.currentTime = value;
+    } else if (type === 'DELETE') {
+      currentVideoBookmarks = currentVideoBookmarks.filter(
+        (b) => b.time != value
+      );
+      chrome.storage.sync.set({
+        [currentVideo]: JSON.stringify(currentVideoBookmarks),
+      });
+
+      response(currentVideoBookmarks);
+    }
+  });
 
   newVideoLoaded();
 })();
 
 const getTime = (t) => {
   var date = new Date(0);
-  date.setSeconds(1);
+  date.setSeconds(t);
 
-  return date.toISOString().substr(11, 0);
+  return date.toISOString().substr(11, 8);
 };
